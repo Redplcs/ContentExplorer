94afc1cc691cef628d2b7442669d33f77605f4d0
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-12-10 02:50:33 +0300

    Throw ArgumentException if trying to set empty values inside path property

---
+++ src/Formats/Pak/src/PakEntry.cs
@@ -0,0 +1,32 @@
+﻿namespace CommandoTools.ContentExplorer.Formats.Pak;
+
+public sealed class PakEntry
+{
+	private PakFileMetadata _metadata;
+
+	/// <summary>
+	/// For testing purposes.
+	/// </summary>
+	internal PakEntry(PakFileMetadata metadata = default)
+	{
+
+	}
+
+	public string Path
+	{
+		get => _metadata.Path!;
+		set
+		{
+			ArgumentException.ThrowIfNullOrWhiteSpace(value, nameof(value));
+
+			_metadata.Path = value;
+		}
+	}
+
+
+	public int Length
+	{
+		get => _metadata.Length;
+		set => _metadata.Length = value;
+	}
+}
---
+++ src/Formats/Pak/tests/PakEntryTests.cs
@@ -0,0 +1,22 @@
+﻿namespace CommandoTools.ContentExplorer.Formats.Pak.Tests;
+
+public class PakEntryTests
+{
+	[Theory]
+	[InlineData(null)]
+	[InlineData("")]
+	[InlineData("  ")]
+	[InlineData("\t")]
+	[InlineData("\n")]
+	public void Path_ForNullOrWhiteSpaceValues_ThrowsArgumentException(string? value)
+	{
+		var entry = new PakEntry();
+
+		void Act()
+		{
+			entry.Path = value!;
+		}
+
+		Assert.ThrowsAny<ArgumentException>(Act);
+	}
+}
